ab120072b68cd4b4a1a7399f4e877a5cc8fe239a
JhonSGIzquierdo <metalprime57@gmail.com>
2023-11-14 17:28:45 -0500

    Customers Page Visualization
    Add Pagination
    Add Table Visualization
    Add Skeleton

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,40 @@
+import Pagination from '@/app/ui/invoices/pagination';
 import { Metadata } from 'next';
- 
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchCustomersPages, fetchFilteredCustomers } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+
+
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
 
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const customers = await fetchFilteredCustomers(query, currentPage);
+
+  const totalPages = await fetchCustomersPages(query);
+
+
+  return (
+    <div className='w-full'>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
+}
